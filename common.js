94a2eb948606016337b90c0e4930168a9dc1484e
mayurichincholkar <91871423+mayurichincholkar@users.noreply.github.com>
2021-12-31 18:14:54 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,20 @@
+var counter=0;
+$(document).bind("mousewheel",function(event){
+	if(event.originalEvent.wheelDelta > 0){
+		counter += 10
+		console.log(counter)
+		$(".gallery_box_outer").css({
+			"transform":"perspective(1000px) rotateX(-8deg) rotateY("+counter+"deg);"
+		});
+
+	}else{
+		counter -= 10
+		console.log(counter)
+		$(".gallery_box_outer").css({
+			"transform":"perspective(1000px) rotateX(-8deg) rotateY("+counter+"deg);"
+		});
+
+
+	}
+
+});
